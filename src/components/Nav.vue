ab84e05b76202d1fac1335a290d8cbdb8dfce87b
Honjes <samuel.papke@protonmail.com>
2022-03-22 06:38:27 +0100

    feat: add navbar

---
+++ src/components/Nav.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav>
+    <div class="link" v-for="route in routes" :key="route.name">
+      <Button label="Primary" :class="{ 'p-button-text': $route.name !== route.name, 'p-button-raised': $route.name === route.name}" @click.prevent="sendTo(route.path)">
+        <router-link :to="route.path">{{route.name}}</router-link>
+      </Button>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+export default {
+  computed: {
+    routes() {
+      return this.$router.getRoutes()
+    },
+    currentRoute() {
+      return this.$route
+    }
+  },
+  methods: {
+    sendTo(path: string) {
+      this.$router.push(path)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+nav {
+  background-color: var(--surface-300);
+  padding: 5px;
+  display: flex;
+  button.p-button {
+    margin-right: 10px;
+  }
+  a {
+    margin-right: 5px;
+    text-decoration: none;
+    text-transform: capitalize;
+    &.router-link-active {}
+  }
+}
+</style>
---
+++ src/sites/Showcase.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import Nav from "../components/Nav.vue"
+</script>
+
+<template>
+  <div>
+    <Nav />
+  </div>
+</template>
